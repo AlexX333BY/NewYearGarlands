2f262dd53ccf14f7cf8306c77532fb871018cff7
Aliaksei <alexx333by@protonmail.com>
2019-02-15 13:39:48 +0300

    Lamp redraw implemented

--- client/GarlandLampFrame.cpp
+++ client/GarlandLampFrame.cpp
@@ -3,6 +3,9 @@
 
 namespace NewYearGarlands
 {
+	const wxString noServerMessage("No server is running now");
+	const wxString disconnectedMessage("Server disconnected");
+
 	VOID SendMessageEvent(GarlandLampFrame *glfFrame, GarlandMessage *gmMessage)
 	{
 		GarlandMessageEvent gmeEvent(glfFrame->GetId(), 
@@ -12,42 +15,76 @@ namespace NewYearGarlands
 		glfFrame->ProcessWindowEvent(gmeEvent);
 	}
 
+	VOID SendCloseEvent(GarlandLampFrame *glfFrame, bool bEverConnected)
+	{
+		LampCloseEvent lceEvent(glfFrame->GetId(), LAMP_CLOSE_EVENT, bEverConnected);
+		lceEvent.SetEventObject(glfFrame);
+		glfFrame->ProcessWindowEvent(lceEvent);
+	}
+
 	DWORD WINAPI PipeListeningThread(GarlandLampFrame *glfFrame)
 	{
 		ClientGarlandPipe cgpPipe;
 		GarlandMessage gmMessage;
-
-		cgpPipe.ConnectToServer();
+		
+		bool bEverConnected = cgpPipe.ConnectToServer();
 		while (cgpPipe.ReadGarlandMessage(&gmMessage))
 		{
 			SendMessageEvent(glfFrame, &gmMessage);
 		}
 
-		glfFrame->Close(true);
+		SendCloseEvent(glfFrame, bEverConnected);
 		return 0;
 	}
 
-	GarlandLampFrame::GarlandLampFrame(const wxString& title) : wxFrame(NULL, -1, title)
+	GarlandLampFrame::GarlandLampFrame(const wxString& title, byte bPercentIndent) : 
+		wxFrame(NULL, -1, title, wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_STYLE | wxFULL_REPAINT_ON_RESIZE),
+		m_bPercentIndent(bPercentIndent), m_currentLampColour(GetBackgroundColour())
 	{
-		m_hPipeListeningThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)PipeListeningThread, this, 0, NULL);
-		Bind(wxEVT_CLOSE_WINDOW, &GarlandLampFrame::OnClose, this, GetId());
-		Bind(LIGHT_UP_EVENT, &GarlandLampFrame::OnLightUp, this, GetId());
+		Bind(LAMP_CLOSE_EVENT, &GarlandLampFrame::OnClose, this);
+		Bind(LIGHT_UP_EVENT, &GarlandLampFrame::OnLightUp, this);
 		Bind(LIGHT_OUT_EVENT, &GarlandLampFrame::OnLightOut, this);
+		Bind(wxEVT_PAINT, &GarlandLampFrame::OnPaint, this);
+		m_hPipeListeningThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)PipeListeningThread, this, 0, NULL);
 	}
 
-	void GarlandLampFrame::OnClose(wxCloseEvent& event)
+	void GarlandLampFrame::OnClose(LampCloseEvent& event)
 	{
 		CloseHandle(m_hPipeListeningThread);
-		event.Skip();
+		if (event.EverConnected())
+		{
+			wxMessageBox(disconnectedMessage);
+		}
+		else
+		{
+			wxMessageBox(noServerMessage);
+		}
+		Close(true);
 	}
 
 	void GarlandLampFrame::OnLightOut(GarlandMessageEvent& event)
 	{
-
+		wxColour colour(GetBackgroundColour());
+		m_currentLampColour = colour;
+		Refresh();
 	}
 
 	void GarlandLampFrame::OnLightUp(GarlandMessageEvent& event)
 	{
+		wxColour colour(event.GetColor());
+		m_currentLampColour = colour;
+		Refresh();
+	}
 
+	void GarlandLampFrame::OnPaint(wxPaintEvent& event)
+	{
+		wxSize windowSize = GetClientSize();
+		wxPoint centerPoint(windowSize.GetWidth() / 2, windowSize.GetHeight() / 2);
+		wxPaintDC wxDc(this);
+		wxBrush brush(m_currentLampColour), oldBrush = wxDc.GetBrush();
+		wxDc.SetBrush(brush);
+		wxDc.DrawCircle(centerPoint, min(windowSize.GetHeight(), windowSize.GetWidth()) * (100 - 2 * m_bPercentIndent) / 100 / 2);
+		wxDc.SetBrush(oldBrush);
+		event.Skip();
 	}
 }
--- client/GarlandLampFrame.h
+++ client/GarlandLampFrame.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "GarlandMessageEvent.h"
+#include "LampCloseEvent.h"
 #include <wx/wxprec.h>
 #ifndef WX_PRECOMP
 	#include <wx/wx.h>
@@ -11,11 +12,14 @@ namespace NewYearGarlands
 	class GarlandLampFrame : public wxFrame
 	{
 	public:
-		GarlandLampFrame(const wxString& title);
+		GarlandLampFrame(const wxString& title, byte bPercentIndent = 10);
 	protected:
-		void OnClose(wxCloseEvent& event);
+		void OnClose(LampCloseEvent& event);
 		void OnLightUp(GarlandMessageEvent& event);
 		void OnLightOut(GarlandMessageEvent& event);
+		void OnPaint(wxPaintEvent& event);
+		const byte m_bPercentIndent;
+		wxColour m_currentLampColour;
 		HANDLE m_hPipeListeningThread;
 	};
 }
